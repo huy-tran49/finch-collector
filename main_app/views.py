531117c7b0c366fceef606fd1f29c6951f4ce765
Huy <h.qt193@gmail.com>
2023-03-03 09:04:05 -0500

    finished part 2

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
+from .models import Finch
 # Create your views here.
+
 
 # Define the home view
 def home(request):
@@ -9,3 +10,11 @@ def home(request):
 
 def about(request):
   return render(request, 'about.html')
+
+def finches_index(request):
+  finches = Finch.objects.all()
+  return render(request, 'finches/index.html', {'finches': finches})
+
+def finch_detail(request, finch_id):
+  finch = Finch.objects.get(id=finch_id)
+  return render(request, 'finches/detail.html', {'finch': finch})
