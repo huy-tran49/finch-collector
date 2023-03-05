f9287980df61dcab00a8c5e42f0e20370d822971
Huy <h.qt193@gmail.com>
2023-03-04 19:39:54 -0500

    add toys

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,9 +1,23 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('finches/', views.finches_index, name='index'),
-    path('finches/<int:finch_id>', views.finch_detail, name='detail'),
+    path('finches/<int:pk>/update/', views.FinchUpdate.as_view(), name='finch_update'),
+    path('finches/<int:pk>/delete/', views.FinchDelete.as_view(), name='finch_delete'),
+    path('finches/create/', views.FinchCreate.as_view(), name='finch_create'),
+    path('finches/<int:finch_id>/add_feeding/', views.add_feeding, name='add_feeding'),
+    path('finches/<int:finch_id>/', views.finch_detail, name='detail'),
+
+    path('finches/<int:finch_id>/assoc_toy/<int:toy_id>/', views.assoc_toy, name='assoc_toy'),
+    path('finches/<int:finch_id>/unassoc_toy/<int:toy_id>/', views.unassoc_toy, name='unassoc_toy'),
+
+    path('toys/', views.ToyList.as_view(), name='toy_index'),
+    path('toys/create/', views.ToyCreate.as_view(), name='toy_create'),
+    path('toys/<int:pk>/update/', views.ToyUpdate.as_view(), name='toy_update'),
+    path('toys/<int:pk>/delete/', views.ToyDelete.as_view(), name='toy_delete'),
+    path('toys/<int:pk>/', views.ToyDetail.as_view(), name='toy_detail'),
 ]
